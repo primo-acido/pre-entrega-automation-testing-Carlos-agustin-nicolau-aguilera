9f70020bf685fb0efbb5b9c56f6e44cb0537139e
agustin.nicolau <agustin.nicolau@gmail.com>
2025-10-26 11:41:53 -0500

    Ultimo set de pruebas antes de entregar y generacion de reporte html

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 import time
+from selenium.webdriver.chrome.options import Options
+
+
 
 
 URL = 'https://www.saucedemo.com/'
@@ -21,8 +23,7 @@ def get_driver():
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service)
 
-
-    time.sleep(10)
+    time.sleep(5)    
 
     return driver
 
@@ -33,14 +34,14 @@ def login_saucedemo(driver):
     driver.get(URL)
 
     # Ingresar credenciales para hacer login
-    driver.find_element(By.NAME, 'user-name').send_keys(USERNAME)
 
-    time.sleep(10)
+    
+    driver.find_element(By.NAME, 'user-name').send_keys(USERNAME)
     driver.find_element(By.NAME, 'password').send_keys(PASSWORD)
     driver.find_element(By.ID, 'login-button').click()
 
 
-    time.sleep(10)
+    time.sleep(7)
 
 
 
